b8478bcbda9c6d608ad4eebfee0833b4a2dd2836
AdrijaChau <121791071+AdrijaChau@users.noreply.github.com>
2025-04-23 23:32:42 +0530

    Update preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,21 +6,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def clean_text(text):
     text = re.sub(r"[^a-zA-Z\s]", "", text)
-    text = text.lower().strip()
-    return text
+    return text.lower().strip()
 
 def prepare_data(filepath):
     df = pd.read_csv(filepath)
-    df.dropna(subset=['plot', 'genre'], inplace=True)
-    df['plot'] = df['plot'].apply(clean_text)
+    df.dropna(subset=['Plot', 'Genre'], inplace=True)
+    df['Plot'] = df['Plot'].apply(clean_text)
 
     le = LabelEncoder()
-    df['genre_encoded'] = le.fit_transform(df['genre'])
+    df['genre_encoded'] = le.fit_transform(df['Genre'])
 
     return df, le
 
 def vectorize_text(train_texts, test_texts):
-    tfidf = TfidfVectorizer(max_features=5000)
+    tfidf = TfidfVectorizer(max_features=5000, stop_words='english')
     X_train = tfidf.fit_transform(train_texts)
     X_test = tfidf.transform(test_texts)
     return X_train, X_test, tfidf
